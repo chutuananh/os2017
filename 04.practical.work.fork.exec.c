46f03f73c21dce9d587aa6a7edbcdff3ac96b304
chutuananh <chutuananh2281995@gmail.com>
2017-02-17 23:14:35 +0700

    Update 04.practical.work.fork.exec.c

--- 04.practical.work.fork.exec.c
+++ 04.practical.work.fork.exec.c
@@ -3,22 +3,24 @@
  int main() 
  {
      printf("Main before fork()\n");
-     int pid = fork();
-     if (pid == 0) {
+     //The first child process 
+     int child_pid_1 = fork();
+     if (child_pid_1 == 0) {
 	printf("I am child after fork(), launching ps -ef\n");
 	char *args[]= { "/bin/ps", "-ef" , NULL};
 	execvp("/bin/ps", args);
 	printf("Finished launching ps -ef\n");
      }
-     else printf("I am parent after fork(), child is %d\n", pid);
+     else printf("I am parent after fork(), child is %d\n", child_pid_1);
 	
-     pid = fork();
-     if (pid == 0) {
+     // The second child process	 
+     int child_pid_2 = fork();
+     if (child_pid_2 == 0) {
 	printf("I am child after fork(), launching free -h\n");
 	char *args[]= { "free", "-h" , NULL};
 	execvp("free", args);
 	printf("Finished launching free -h\n");
      }
-     else printf("I am parent after fork(), child is %d\n", pid);
+     else printf("I am parent after fork(), child is %d\n", child_pid_2);
      return 0;
  } 
